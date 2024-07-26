cf496e7d67b6453b40f8e7175bb465ee7af399f3
Tran Quang Huy <Tran Quang Huy>
2024-07-26 17:09:56 +0700

    26/07/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/futuredemo/future_builder.dart';
-import 'package:learn_flutter/futuredemo/future_group.dart';
-import 'package:learn_flutter/futuredemo/imagepicker.dart';
-import 'package:learn_flutter/futuredemo/splash_screen.dart';
-import 'package:learn_flutter/learn/login/login.dart';
-import 'package:learn_flutter/learn/login_bloc/login_screen.dart';
-import 'package:learn_flutter/learn/noteapp_bloc/screens/note_screen.dart';
-import 'package:learn_flutter/learn/screensaver/screensaver.dart';
-import 'package:learn_flutter/timerdemo/stopwatch.dart';
-import 'package:learn_flutter/widgettree/step_streambuilder.dart';
 
-import 'collections/sets.dart';
-import 'learn/signup/signup.dart';
-import 'widgettree/materialbanner.dart';
+import 'learn/home/homescreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: HomeScreen(),
     );
   }
 }
