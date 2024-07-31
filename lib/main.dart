3370bd1ffae3849d77c377713ed820d830f0519f
Tran Quang Huy <Tran Quang Huy>
2024-07-31 13:57:00 +0700

    31/007/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/dio_retrofit/screen/coin_screen.dart';
 import 'package:learn_flutter/setting/ui/custom_scrollview.dart';
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CustomScrollviewScreen(),
+      home: CoinListScreen(),
     );
   }
 }
